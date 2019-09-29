15d9e3b46747c6db54753b7b6bcbd697b61ac2f9
aebonilla2019 <54388456+aebonilla2019@users.noreply.github.com>
2019-09-29 00:53:37 -0700

    Add files via upload

---
+++ PyBank/PyBank.py
@@ -0,0 +1,92 @@
+#import dependecies
+import os
+
+#module for reading CSV files
+import csv
+import math
+
+budget_file = os.path.join('budget_data.csv')
+output_file = os.path.join('budget_data_analysis.txt')
+
+#using csv module to read data
+with open(budget_file, newline ='') as csvfile:
+    
+    #csv reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    
+    print("Financial Analysis")
+    print("---------------------------")
+    
+    #Counter for the total of the months
+  
+    header = next(csvreader)
+#     data = list(csvreader)
+#     row_count = len(data)
+    
+    #Define 'month_total' varible to store the value and set it to 0
+    month_total = 0   
+    
+    
+    #Define 'net_change' varible and set to 0
+    net_change = 0   
+    
+    
+    
+    #Create greatest_change dictionary: this includes the increase value key and value, the increase date key, drecrease value key and value, and decrease date key
+    greatest_change = {'increase_value': 0,'increase_date': '','decrease_value': 0,'decrease_date': ''  }
+       
+    #create empty results list
+    results = []    
+    #print ("Total Months: " + str(row_count))
+    
+    
+    #loop through each line of the csv file (per the variable csvreader)
+    for line in csvreader:  
+        #add 1 to 'month_total' value for each row
+        month_total += 1   
+        #add the value in column 2 (Profit/Loss) to the net change variable
+        net_change += int(line[1]) 
+        
+        #conditional if profit/loss value is greater than the increase value set the greater value as the value for the increase value variable
+        #and set the date value as the increase date
+        if int(line[1]) > greatest_change['increase_value']:
+            
+            greatest_change['increase_value'] = int(line[1])   
+            
+            greatest_change['increase_date'] = line[0]   #... set "Date" value from file as value for 'increase_date' key ...
+        
+        #otherwise if the profit/loss value is less than the decrease value set the lesser value as the decrese value variable
+        #and set the date value as the decrease date
+        elif int(line[1]) < greatest_change['decrease_value']:   
+            
+            greatest_change['decrease_value'] = int(line[1])   
+            
+            greatest_change['decrease_date'] = line[0]   
+       
+    
+    
+    
+   #populate the results list created earlier   
+    results = [\
+        'Total Months: ' + str(month_total) + '\n', \
+        'Total: $' + str(net_change) + '\n', \
+        'Average Change: $' + str(net_change/month_total) + '\n', \
+        'Greatest Increase in Profits: ' + greatest_change["increase_date"] + ' ($'+str(greatest_change["increase_value"]) + '\n', \
+        'Greatest Decrease in Profits: ' + str(greatest_change["decrease_date"]) + ' ($' + str(greatest_change["decrease_value"]) + ')' \
+    ]
+    
+    
+    total = 0
+    for row in csvreader:
+        total += int(row[1])
+  #open text file and write the results      
+with open(output_file, mode='w') as txtFile:   
+    txtFile.writelines(results)   
+  #open text file and print results
+with open(output_file, mode='r') as txtFile:   #Open text write file in read mode and set to 'txtFile' variable
+    print(txtFile.read())   #print contents of text write file to console
+    
+
+        
+print (total)
+print(results)
---
+++ PyRoll/PyRoll.py
@@ -0,0 +1,77 @@
+#import dependencies
+import os   
+import csv   
+
+#csv variable pointing to csv file
+data_file = os.path.join('resources','election_data.csv')
+
+
+#write file variable
+write_file = os.path.join('Election_Results.txt')
+
+#with csv folder open set set variable for CSV reder
+with open(data_file, mode='r') as csv_file:  
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    
+    #Define header row
+    header = next(csv_reader)
+    
+    #Define total_votes variable
+    total_votes = 0   #Define 'totalVote' variable and set it to 0
+    
+    #Create the results list dictionary
+    results_list = {}
+    
+    #create a winner dictionary
+    winner_dict = {'name': '', 'votes': 0}
+    
+    
+    #loop through the csv_reader variable by line
+    for line in csv_reader:   
+        
+        #add one to the total_votes
+        total_votes += 1   
+        
+        # if the candidate name is not in the dictionary as a key add it tot he dictionary along with the number of vote and vote percentage
+        if line[2] not in results_list.keys():   
+            results_list[line[2]] = {'num_votes': 0, 'percentVotes': 0}   
+        
+        # if the candidate is in the dictionary add 1 to the votes for that candidate
+        if line[2] in results_list.keys():   
+            results_list[line[2]]['num_votes'] += 1   
+    
+    #set results list that contains total_votes
+    resultsSummary = ['Total Votes: ' + str(total_votes) + '\n']   
+    
+    
+    #loop through each candidates name in the results list
+    for candidate in results_list.keys():   
+        
+        #set percentvote as num_votes devided by total_votes *100
+        results_list[candidate]['percentVotes'] = int(results_list[candidate]['num_votes'] / total_votes * 100)   
+        
+        #if num_votes key value is greater than votes value of the winner dictionary se the name in the winner dictionary to current candidate name
+        #and set the votes key value of the winner dictionary to the num_votes key value
+        if results_list[candidate]['num_votes'] > winner_dict['votes']:   
+            
+            winner_dict['name'] = candidate   
+            
+            winner_dict['votes'] = results_list[candidate]['num_votes']
+                                   
+        #Append to 'resultsSummary' list: current candidate's name, 'percentVotes' key value , and 'num_votes' key value
+        resultsSummary.append(\
+            candidate + ': ' + \
+            "{:.2f}".format(results_list[candidate]['percentVotes']) + '% (' + \
+            str(results_list[candidate]['num_votes']) + ') ')
+
+    
+    #Append to 'resultsSummary' list: 'name' key value of 'winner' dict
+    resultsSummary.append('\nWinner: ' + winner_dict['name'])   
+    
+    #with write_file open write each line of resultssummary to the file
+    with open(write_file, mode='w') as txtFile:   
+        txtFile.writelines(resultsSummary)   
+
+    #print results of the text file
+    with open(write_file, mode='r') as txtFile:   
+        print(txtFile.read())   
